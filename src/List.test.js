249983a37a5b0043e251efe9f80925ec01edddec
George Apazidis <geoapa@gmail.com>
2019-09-27 11:09:25 -0400

    added test scripts for Card and List components. ERROR ON LIST > Review with Alfredo

---
+++ src/List.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import List from "./List";
+import Card from "./Card";
+
+describe("List component", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<List />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // it("renders the UI as expected", () => {
+  //   const tree = renderer
+  //     .create(<Card key="8976976" title="some title" content="some content" />)
+  //     .toJSON();
+  //   expect(tree).toMatchSnapshot();
+  // });
+});
